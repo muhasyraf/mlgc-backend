b49c8cae9823cb1bf9ba1cf668d9e7bda97790cb
muhasyraf <faizkamil39@gmail.com>
2024-12-07 20:59:03 +0700

    GOD HELP ME

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ const InputError = require("../exceptions/InputError");
 (async () => {
   try {
     const server = Hapi.server({
-      port: process.env.PORT || 8080,
+      port: 8080,
       host: "0.0.0.0",
       routes: {
         cors: {
